604bd0e7a42169e7be5757073ada9f1a54879582
pushittothecloud <pushittothecloud@users.noreply.github.com>
2026-04-02 21:11:49 -0600

    Configure GitHub Pages deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import TrainingMode from './pages/TrainingMode';
 import Progress from './pages/Progress';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/training/:mode" element={<TrainingMode />} />
         <Route path="/progress" element={<Progress />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
